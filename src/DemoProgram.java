ef0b6ef1205a7f591c50f542983edfcab0675f67
XinJin Kek <kekxinjin@gmail.com>
2021-05-22 18:27:40 +0800

    modified first payment method to payment method try, added another class payment method without form GUI

--- src/DemoProgram.java
+++ src/DemoProgram.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class DemoProgram {
     public static void main(String[] args) {
-        JFrame frame = new paymentMethod("Payment via Credit/Debit card");
+        JFrame frame = new paymentMethodTry("Payment via Credit/Debit card");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 180);
         frame.setVisible(true);
---
+++ src/paymentMethodTry.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+
+class paymentMethodTry extends JFrame{
+
+    //declaration
+    private JPanel mainPanel;
+    private JTextField cardNumberTextField;
+    private JTextField expiryDateTextField;
+    private JButton confirmButton;
+    private JLabel validateLabel, expiryDateLabel, cardNumberLabel;
+
+    public paymentMethodTry(String title)
+    {
+        super (title);
+        this.setContentPane(mainPanel);
+        this.pack();
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        validateLabel = new JLabel("Invalid number");
+        validateLabel.setText("Invalidate");
+
+    }
+
+
+
+    //credit card number validation
+    //base on the Luhn Checksum algorithm
+    public boolean cardNumberValidation(String number)
+    {
+        int sum = 0;
+        for(int i=0; i<number.length(); i++)
+        {
+            int digit = Integer.valueOf(number.substring(i, i+1));
+            if(i % 2 == 0)
+            {
+                digit *= 2;
+                sum += (digit / 10) + (digit % 10);
+            }
+            else
+            {
+                sum += digit;
+            }
+        }
+        return (sum % 10 == 0);
+    }
+
+
+
+}
