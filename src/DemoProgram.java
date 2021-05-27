8e058473e031d24358730934e5b964548136c0ae
XinJin Kek <kekxinjin@gmail.com>
2021-05-27 21:39:58 +0800

    added payment in.form and added display in .form

--- src/DemoProgram.java
+++ src/DemoProgram.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class DemoProgram {
     public static void main(String[] args) {
+
+        JFrame method = new payTryMethod("Pay with MasterCard or Visa");
 
     }
 }
