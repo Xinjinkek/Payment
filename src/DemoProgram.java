dcb034bec5883ebf42b119eed9fd73a228dab2a7
XinJin Kek <kekxinjin@gmail.com>
2021-05-28 20:32:19 +0800

    edited the main

--- src/DemoProgram.java
+++ src/DemoProgram.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 
 public class DemoProgram {
     public static void main(String[] args) {
-        //JFrame register = new JFrame(" Register");
-        JFrame menu = new Menu("Menu");
+        JFrame register = new Register(" Register");
+        //JFrame menu = new Menu("Menu");
         //JFrame method = new payTryMethod("Pay with MasterCard or Visa");
 
     }
