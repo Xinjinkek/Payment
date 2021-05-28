68ba8cf4ace93ec65ab7c4c72ce3b04447c085f6
XinJin Kek <kekxinjin@gmail.com>
2021-05-28 20:21:43 +0800

    added content to the Register.form

--- src/Register.java
+++ src/Register.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class Register {
+    private JPanel mainRegister;
+    private JLabel titleLabel;
+    private JLabel nameLabel;
+    private JLabel contactLabel;
+    private JLabel addressLabel;
+    private JTextField nameTextField;
+    private JTextField contactTextField;
+    private JTextField addressTextField;
+    private JButton registerButton;
 }
