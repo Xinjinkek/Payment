55ebecefbdcf39c3aff82d20b6f9507869061e7f
Ivan Kong Kai Wen <kaiwenkw@hotmail.com>
2021-05-29 22:35:01 +0800

    table content

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -53,8 +55,26 @@ public class Menu extends JFrame {
     private JButton backButton;
     private JTable tableOrder;
     private JPanel panelTotal;
-    DefaultTableModel dtm;
+    public DefaultTableModel dtm;
 
+    double f1, f2, f3, f4;
+    double food1, food2, food3, food4;
+    double d1, d2, d3, d4;
+    double drinks1, drinks2, drinks3, drinks4;
+    double s1, s2, s3, s4, s5, s6;
+    double sides1, sides2, sides3, sides4, sides5, sides6;
+    double total = 0;
+    double totalPrice = 0;
+
+    double foodTotal;
+    double drinksTotal;
+    double sidesTotal;
+
+    final static int numberOfFoods = 4;
+    final static int numberOfDrinks = 4;
+    final static int numberOfSides = 6;
+
+    final static double food1price = 4.0;
     public Menu(String title) {
         //super(title);
         frame = new JFrame(title);
@@ -79,7 +99,79 @@ public class Menu extends JFrame {
             frame.dispose();
             }
         });
+
+        spinnerChickenBurger.addChangeListener(listener);
+        spinnerBeefBurger.addChangeListener(listener);
     }
+
+    ChangeListener listener = new ChangeListener() {
+        @Override
+        public void stateChanged(ChangeEvent e) {
+
+            final int quantity = (int) ((JSpinner) e.getSource()).getValue();
+            final int rows = tableOrder.getRowCount();
+            for (int row = 0; row < rows; row++) {
+                if (dtm.getValueAt(row, 3) == e.getSource()) {
+                    if (dtm.getValueAt(row, 0).equals("Chicken Burger")) {
+                        dtm.setValueAt(quantity, row, 1); // obj, row, column
+                        dtm.setValueAt(f1 * quantity, row, 2);
+                        food1 = f1 * quantity;
+
+                    } else if (dtm.getValueAt(row, 0).equals("Beef Burger")) {
+
+                        dtm.setValueAt(quantity, row, 1);
+                        dtm.setValueAt(f2 * quantity, row, 2);
+                        food2 = f2 * quantity;
+                    } else if (dtm.getValueAt(row, 0).equals("Benjo")) {
+
+                        dtm.setValueAt(quantity, row, 1);
+                        dtm.setValueAt(f3 * quantity, row, 2);
+                        food3 = f3 * quantity;
+                    } else if (dtm.getValueAt(row, 0).equals("Hotdog")) {
+
+                        dtm.setValueAt(quantity, row, 1);
+                        dtm.setValueAt(f4 * quantity, row, 2);
+                        food4 = f4 * quantity;
+                    }
+
+                    if (quantity == 0) {
+                        dtm.removeRow(row);
+                    }
+                    foodTotal = food1 + food2 + food3 + food4;
+                    total = foodTotal + drinksTotal + sidesTotal;
+                    textFieldTotal.setText(total + "");
+                    return;
+                }
+           }
+
+            // there was no row with this JSpinner, so we have to add it
+            for (int i = 0; i < numberOfFoods; i++) {
+                // looking for the "clicked" JSpinner
+                if (spinnerChickenBurger == e.getSource()) {
+                    totalPrice = food1price;
+                    f1 = 4.0;
+                    food1 =f1;
+                    foodTotal = food1 + food2 + food3 + food4;
+                    total = foodTotal + drinksTotal + sidesTotal;
+                    textFieldTotal.setText(total + "");
+                    dtm.addRow(new Object[] { labelChickenBurger.getText(), quantity, totalPrice, spinnerChickenBurger });
+                    return;
+                }
+                else if (spinnerBeefBurger == e.getSource()) {
+                  //  totalPrice = foodprice;
+                    f2 = 5.0;
+                    food2 =f2;
+                    foodTotal = food1 + food2 + food3 + food4;
+                    total = foodTotal + drinksTotal + sidesTotal;
+                    textFieldTotal.setText(total + "");
+                    dtm.addRow(new Object[] { labelBeefBurger.getText(), quantity, f2*quantity, spinnerBeefBurger });
+                    return;
+                }
+
+
+            }
+        }
+    };
 
      void createUIComponents() {
         // TODO: place custom component creation code here
