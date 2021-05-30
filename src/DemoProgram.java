2ce5ac06e6aa7842b831384200ae706b070ad5f3
Ivan Kong Kai Wen <kaiwenkw@hotmail.com>
2021-05-31 00:08:50 +0800

    im stronk

--- src/DemoProgram.java
+++ src/DemoProgram.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 
 public class DemoProgram {
     public static void main(String[] args) throws IOException {
-       // JFrame main = new MainMenu("Welcome");
-        JFrame register = new Register("Register");
+        JFrame main = new MainMenu("Welcome to Ramly.Jr House");
     }
 }
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,43 +1,54 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class MainMenu extends JFrame {
-    private JPanel mainPanel = new JPanel();
+    JPanel mainPanel = new JPanel();
     public MainMenu(String title) throws IOException {
         super(title);
+        createUIComponents();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
-        this.setSize(1000,1000);
+        this.setSize(350,450);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-        createUIComponents();
+
     }
 
     public void createUIComponents() throws IOException {
-        JPanel panel = new JPanel(new BorderLayout());
-        Image image = ImageIO.read(this.getClass().getResource("logo.jpg"));
+        mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBackground(Color.decode("#EDBB99"));
+        Image image = ImageIO.read(getClass().getResource("mainlogo.png"));
         Image imageSize = image.getScaledInstance(350,300,Image.SCALE_SMOOTH);
         ImageIcon icon = new ImageIcon(imageSize);
         JLabel picture = new JLabel(icon);
-        Box right = Box.createVerticalBox();
+        Box box = Box.createVerticalBox();
         JPanel panel1 = new JPanel();
-        JLabel title = new JLabel("898 Food Restaurant");
+        panel1.setBackground(Color.decode("#EDBB99"));
+        JLabel title = new JLabel("Ramly.Jr House");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
-        title.setAlignmentY(0.0f);
         title.setHorizontalAlignment(SwingConstants.CENTER);
-        title.setFont(new Font("Serif", Font.ITALIC + Font.BOLD, 18));
-        title.setForeground(Color.BLUE);
+        title.setFont(new Font("INK FREE", Font.BOLD, 18));
+        title.setForeground(Color.BLACK);
 
         JButton button = new JButton("Order Food Now >>");
         panel1.add(button);
         button.setAlignmentX(Component.LEFT_ALIGNMENT);
 
-        panel.add(picture, BorderLayout.CENTER);
-        panel.add(right, BorderLayout.SOUTH);
-        right.add(title);
-        right.add(panel1);
-        add(panel);
+        mainPanel.add(picture, BorderLayout.CENTER);
+        mainPanel.add(box, BorderLayout.SOUTH);
+        box.add(title);
+        box.add(panel1);
+        add(mainPanel);
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new Register("Register");
+                dispose();
+            }
+        });
     }
 }
