ed18a8246026a9f7e65f1ab921bd53b9383aee93
XinJin Kek <kekxinjin@gmail.com>
2021-05-30 19:57:52 +0800

    idk

--- src/DemoProgram.java
+++ src/DemoProgram.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class DemoProgram {
     public static void main(String[] args) {
         JFrame register = new Register("Register");
+        //JFrame logo = new Logo("Logo");
         //Menu menu = new Menu("Main Menu");
         //JFrame method = new PaymentMethod("Pay with MasterCard or Visa");
 
